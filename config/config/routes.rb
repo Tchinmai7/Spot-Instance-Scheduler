0e3ad69a06e34ffbce56f26724cf2b93ba8d1b20
Tarun Chinmai <tchinmai7@gmail.com>
2017-03-16 00:25:54 +0530

    rolling back

---
+++ config/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :jobs
+  resources :aws_keys
+  resources :bucket_configs
+  get 'welcome/home'
+  root 'welcome#home'
+  devise_for :users, controllers: { registrations: "registrations" }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
