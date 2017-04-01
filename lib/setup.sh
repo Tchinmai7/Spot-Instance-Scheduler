e5b9ba56ff4aed57af132f4285380aafb8353efc
Tarun Chinmai <tchinmai7@gmail.com>
2017-04-01 23:15:56 +0530

    adding background runs for image

--- lib/setup.sh
+++ lib/setup.sh
@@ -2,4 +2,5 @@
 $image=$1
 $command=$2
 curl -sSL https://get.docker.com/ | sh
-docker run $command $image
+#echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock --mtu=1450  --dns 8.8.8.8 --experimental=true\"" >/etc/default/docker
+docker run -d $command $image
